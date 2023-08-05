47bab2e59e071b8a58b17af5f91857b5ffbc4ffb
Tristan Biesemeier <67628766+trbie@users.noreply.github.com>
2023-08-04 17:11:22 -0700

    finish ui

--- frontend/src/pages/novel-list.tsx
+++ frontend/src/pages/novel-list.tsx
@@ -1,8 +1,7 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import Link from "next/link";
+import { Image, Loader, Text, Title } from "@mantine/core";
 import { useQuery } from "@tanstack/react-query";
-import { BackgroundImage, Card, Image, Loader, Text } from "@mantine/core";
+import axios from "axios";
+import Link from "next/link";
 
 export default function NovelList() {
     const { data: response, isLoading } = useQuery(["hot-novels"], async () => {
@@ -16,8 +15,10 @@ export default function NovelList() {
                     <Loader className="m-auto" size="xl" />
                 </div>
             )) || (
-                <div className="max-w-2/5 w-3/5 mt-5 mx-auto p-4 rounded-md bg-neutral-800">
-                    <h1>Featured</h1>
+                <div className="max-w-2/5 w-3/5 m-5 mx-auto p-4 rounded-md bg-neutral-800">
+                    <Title className="mb-4 -m-4 p-4 rounded-t-md bg-neutral-950" size="38px">
+                        Featured
+                    </Title>
                     <div className="flex flex-wrap justify-center">
                         {response.data.map((novel, index) => {
                             const novelSlug = novel.url.split("/").pop();
@@ -52,9 +53,9 @@ const BookCard = ({ title, image }: BookCardProps) => {
 
         //     <Text align="center">{title}</Text>
         // </Card>
-        <div className="rounded-md border border-neutral-500 relative">
-            <Image src={image} width={200} height={300} radius="sm" withPlaceholder />
-            <Text className="rounded-md text-center absolute bottom-0 w-full p-1 bg-neutral-800">
+        <div className="rounded-md border-neutral-500 relative">
+            <Image src={image} width={200} height={300} radius={6} withPlaceholder />
+            <Text className="rounded-b-md border-t-2 border-neutral-500 text-center absolute bottom-0 w-full p-1 bg-neutral-950">
                 {title}
             </Text>
         </div>
