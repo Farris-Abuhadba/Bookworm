84f7ab8228a041e5c79063999ae3f88b3d8b0d92
Abuhadba-Farris <57422694+Farris-Abuhadba@users.noreply.github.com>
2023-08-10 09:46:35 -0700

    Added next button

--- src/pages/novel/[novel]/[chapter].tsx
+++ src/pages/novel/[novel]/[chapter].tsx
@@ -4,38 +4,47 @@ import LoadingScreen from "../../../components/LoadingScreen";
 import { Chapter } from "../../../types/Novel";
 
 export default function ChapterContent() {
-    const router = useRouter();
-    const { novel, chapter } = router.query;
+  const router = useRouter();
+  const { novel, chapter } = router.query;
 
-    if (novel == undefined || chapter == undefined)
-        return <LoadingScreen backUrl={"/novel/" + novel} />;
+  if (novel == undefined || chapter == undefined)
+    return <LoadingScreen backUrl={"/novel/" + novel} />;
 
-    const [chapterData, setChapterData] = useState<Chapter>();
-    const [isLoading, setIsLoading] = useState(true);
+  const [chapterData, setChapterData] = useState<Chapter>();
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        fetch(`/api/chapter?novelId=${novel}&chapterId=${chapter}`)
-            .then((response) => response.json())
-            .then((data) => {
-                setChapterData(data);
-                setIsLoading(false);
-            })
-            .catch((error) => {
-                console.error("Error fetching novel data:", error);
-                setIsLoading(false);
-            });
-    }, [novel, chapter]);
+  useEffect(() => {
+    fetch(`/api/chapter?novelId=${novel}&chapterId=${chapter}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setChapterData(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching novel data:", error);
+        setIsLoading(false);
+      });
+  }, [novel, chapter]);
 
-    if (isLoading) return <LoadingScreen backUrl={"/novel/" + novel} />;
+  if (isLoading) return <LoadingScreen backUrl={"/novel/" + novel} />;
 
-    return (
-        <div className="bg-neutral-950 w-3/5 m-5 mx-auto rounded-md p-4 px-7">
-            <h1>{chapterData.title}</h1>
-            <div>
-                {chapterData.content.map((text) => {
-                    return <p className="my-4">{text}</p>;
-                })}
-            </div>
-        </div>
-    );
+  const parts = (chapter as string).split("-");
+  const nextChapter = parts[0] + "-" + (parseInt(parts[1], 10) + 1);
+
+  return (
+    <div className="bg-neutral-950 w-3/5 m-5 mx-auto rounded-md p-4 px-7">
+      <h1>{chapterData.title}</h1>
+      <div>
+        {chapterData.content.map((text) => {
+          return <p className="my-4">{text}</p>;
+        })}
+      </div>
+      <a
+        className="text-blue-500 hover:underline"
+        href={`/novel/${novel}/${nextChapter}`}
+      >
+        Next Chapter
+      </a>
+    </div>
+  );
 }
