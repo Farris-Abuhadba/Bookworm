0a3ae424b34a7bc17af65ff4121163858f54367b
Abuhadba-Farris <57422694+Farris-Abuhadba@users.noreply.github.com>
2023-08-12 14:06:23 -0700

    Added Key to iterator Chapter List for Vercel deployment

--- src/components/ChapterList.tsx
+++ src/components/ChapterList.tsx
@@ -2,43 +2,43 @@ import RelativeTime from "@yaireo/relative-time";
 import { Chapter } from "../types/Novel";
 
 interface Props {
-    chapters: Chapter[];
+  chapters: Chapter[];
 }
 
 const ChapterList = ({ chapters }: Props) => {
-    return (
-        <div id="chapter-list" className="p-4 mb-2 rounded-md bg-stone-950">
-            {/* <Title align="center" size="24px">
+  return (
+    <div id="chapter-list" className="p-4 mb-2 rounded-md bg-stone-950">
+      {/* <Title align="center" size="24px">
                 Chapter List
             </Title>
             <Divider className="my-4" size="md" /> */}
-            {chapters.map((chapter, index) => (
-                <ChapterRow index={index + 1} chapter={chapter} />
-            ))}
-        </div>
-    );
+      {chapters.map((chapter, index) => (
+        <ChapterRow key={index} index={index + 1} chapter={chapter} />
+      ))}
+    </div>
+  );
 };
 
 export default ChapterList;
 
 interface ChapterRowProps {
-    index: number;
-    chapter: Chapter;
+  index: number;
+  chapter: Chapter;
 }
 
 const ChapterRow = ({ index, chapter }: ChapterRowProps) => {
-    return (
-        <div key={chapter.title} className="flex">
-            <span className="font-bold w-16 text-neutral-400/50">{index}</span>
-            <a
-                className="grow underline text-neutral-400 hover:text-neutral-200"
-                href={location.href + "/" + chapter.id}
-            >
-                {chapter.title}
-            </a>
-            {/* <span className="text-right text-neutral-400/50">
+  return (
+    <div key={chapter.title} className="flex">
+      <span className="font-bold w-16 text-neutral-400/50">{index}</span>
+      <a
+        className="grow underline text-neutral-400 hover:text-neutral-200"
+        href={location.href + "/" + chapter.id}
+      >
+        {chapter.title}
+      </a>
+      {/* <span className="text-right text-neutral-400/50">
                 {new RelativeTime().from(new Date(chapter.timestamp))}
             </span> */}
-        </div>
-    );
+    </div>
+  );
 };
