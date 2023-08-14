20d8ecd2d47a7af8b279f8f3e41786b0946e8fbb
Tristan Biesemeier <67628766+trbie@users.noreply.github.com>
2023-08-13 22:04:09 -0700

    dont add empty paragraphs

--- src/pages/api/chapter.tsx
+++ src/pages/api/chapter.tsx
@@ -3,35 +3,39 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { Chapter } from "../../types/Novel";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    const { novelId, chapterId } = req.query;
-    console.log(req.query);
+  const { novelId, chapterId } = req.query;
+  console.log(req.query);
 
-    if (novelId == undefined || chapterId == undefined) {
-        res.status(404).json({ error: "Novel or chapter cannot be undefined" });
-        return;
-    }
+  if (novelId == undefined || chapterId == undefined) {
+    res.status(404).json({ error: "Novel or chapter cannot be undefined" });
+    return;
+  }
 
-    try {
-        const response = await fetch(`https://novelusb.com/novel-book/${novelId}/${chapterId}`);
-        const document = new JSDOM(await response.text()).window.document;
+  try {
+    const response = await fetch(
+      `https://novelusb.com/novel-book/${novelId}/${chapterId}`
+    );
+    const document = new JSDOM(await response.text()).window.document;
 
-        var title = document.querySelector("a.chr-title").textContent;
+    var title = document.querySelector("a.chr-title").textContent;
 
-        var content = [];
-        var ps = document.querySelectorAll("#chr-content p");
-        ps.forEach((p) => {
-            content.push(p.textContent);
-        });
+    var content = [];
+    var ps = document.querySelectorAll("#chr-content p");
+    ps.forEach((p) => {
+      if (p && p.textContent != "") content.push(p.textContent);
+    });
 
-        const chapter: Chapter = {
-            title,
-            id: chapterId.toString(),
-            content,
-        };
+    const chapter: Chapter = {
+      title,
+      id: chapterId.toString(),
+      content,
+    };
 
-        res.status(200).json(chapter);
-    } catch (error) {
-        console.error("An error occurred:", error);
-        res.status(500).json({ error: "An error occurred while extracting chapter data." });
-    }
+    res.status(200).json(chapter);
+  } catch (error) {
+    console.error("An error occurred:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while extracting chapter data." });
+  }
 };
