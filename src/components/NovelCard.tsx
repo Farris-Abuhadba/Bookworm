91124868492585e78521f3aed5440f9995e08870
Tristan Biesemeier <67628766+trbie@users.noreply.github.com>
2023-08-21 20:29:25 -0700

    phone ui for novelist

--- src/components/NovelCard.tsx
+++ src/components/NovelCard.tsx
@@ -9,16 +9,15 @@ interface Props {
 
 const NovelCard = ({ title, image, width, height }: Props) => {
   return (
-    <div className={"rounded-md relative"}>
+    <div className="group break-inside-avoid-column mb-5">
       <Image
+        className="rounded-md group-hover:border-neutral-700 border border-transparent"
         alt={title}
         src={image}
-        width={width ? width : 200}
-        height={height ? height : 300}
         radius={6}
         withPlaceholder
       />
-      <Text className="rounded-b-md border-t-2 border-neutral-500 text-center absolute bottom-0 w-full p-1 bg-neutral-950">
+      <Text className="mt-1 px-1 rounded-md group-hover:bg-neutral-700">
         {title}
       </Text>
     </div>
--- src/pages/novel-list.tsx
+++ src/pages/novel-list.tsx
@@ -34,15 +34,15 @@ export default function NovelList() {
   if (isLoading) return <LoadingScreen />;
 
   return (
-    <div className="max-w-2/5 w-3/5 m-5 mx-auto p-4 rounded-md bg-neutral-800">
+    <div className="sm:max-w-2/5 sm:w-3/5 sm:my-5 mx-auto p-4 rounded-md bg-neutral-800">
       <Title className="mb-4 -m-4 p-4 rounded-t-md bg-neutral-950" size="38px">
         Featured
       </Title>
-      <div className="flex flex-wrap justify-center">
+      <div className="columns-2 sm:columns-3 md:columns-4 lg:columns-5">
         {novels.map((novel, index) => {
           const novelSlug = novel.id;
           return (
-            <Link key={index} href={`/novel/${novelSlug}`} className="m-1">
+            <Link key={index} href={`/novel/${novelSlug}`}>
               <NovelCard title={novel.title} image={novel.cover} />
             </Link>
           );
