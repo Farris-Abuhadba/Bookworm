1530d8f2a2e2bdcbf9ea0d4b4954e453ddd1b286
Abuhadba-Farris <57422694+Farris-Abuhadba@users.noreply.github.com>
2023-08-22 06:33:45 -0700

    Added react query to all pages

--- src/pages/novel-list.tsx
+++ src/pages/novel-list.tsx
@@ -4,34 +4,36 @@ import { useEffect, useState } from "react";
 import LoadingScreen from "../components/LoadingScreen";
 import NovelCard from "../components/NovelCard";
 import { Novel } from "../types/Novel";
+import { useQuery } from "react-query";
 
 export default function NovelList() {
-  const [novels, setNovels] = useState<Novel[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
-
   useEffect(() => {
     getNovels();
   }, []);
 
   const getNovels = async () => {
-    try {
-      const response = await fetch("/api/hot-novels", {
-        method: "get",
-        headers: {
-          "content-type": "application/json",
-        },
-      });
+    const response = await fetch("/api/hot-novels", {
+      method: "get",
+      headers: {
+        "content-type": "application/json",
+      },
+    });
 
-      const data = await response.json();
-      setNovels(data);
-      setIsLoading(false);
-    } catch (error) {
-      console.error("Error fetching novel data:", error);
-      setIsLoading(false);
-    }
+    const data = await response.json();
+    return data;
   };
 
+  const {
+    data: novels,
+    error,
+    isLoading,
+  } = useQuery(["hotNovels", getNovels], () => getNovels());
+
   if (isLoading) return <LoadingScreen />;
+
+  if (error) return "Error getting Hot Novels";
+
+  const novelsArray = novels as Novel[];
 
   return (
     <div className="max-w-2/5 w-3/5 m-5 mx-auto p-4 rounded-md bg-neutral-800">
@@ -39,7 +41,7 @@ export default function NovelList() {
         Featured
       </Title>
       <div className="flex flex-wrap justify-center">
-        {novels.map((novel, index) => {
+        {novelsArray.map((novel, index) => {
           const novelSlug = novel.id;
           return (
             <Link key={index} href={`/novel/${novelSlug}`} className="m-1">
