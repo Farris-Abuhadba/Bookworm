c93de24fe2f4d3a4a59e9de5e90a9b54a7f3e6f8
trbie <67628766+trbie@users.noreply.github.com>
2023-09-15 00:36:03 -0700

    create footer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,13 @@
 import { PropsWithChildren } from "react";
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const Layout = ({ children }: PropsWithChildren) => {
-  return <Navbar>{children}</Navbar>;
+  return (
+    <Navbar>
+      {children}
+      <Footer />
+    </Navbar>
+  );
 };
 export default Layout;
