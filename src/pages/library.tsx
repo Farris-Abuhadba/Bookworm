d8a10ce6c72899c3cc996442b36f6be6cf4c2911
Tristan Biesemeier <67628766+trbie@users.noreply.github.com>
2023-09-25 17:28:11 -0700

    finish library list

--- src/pages/library.tsx
+++ src/pages/library.tsx
@@ -8,7 +8,7 @@ const LibraryPage = () => {
   const [novelLibrary, setNovelLibrary] = useState([]);
 
   useEffect(() => {
-    let novels = JSON.parse(localStorage.getItem("novelLibrary")) || [];
+    let novels = getLibrary();
     setNovelLibrary(novels);
   }, []);
 
@@ -18,8 +18,11 @@ const LibraryPage = () => {
         Library
       </Title>
       {novelLibrary.map((novelId, index) => {
-        return <NovelRow novelId={novelId} index={index} />;
+        return <NovelRow key={novelId} novelId={novelId} index={index} />;
       })}
+
+      {novelLibrary.length == 0 &&
+        "Your library is empty, add novels to see your progress and get notified when new chapters are released"}
     </div>
   );
 };
@@ -27,8 +30,13 @@ const LibraryPage = () => {
 export default LibraryPage;
 
 const NovelRow = ({ novelId, index }) => {
+  const [removed, removeRow] = useState(false);
   const { data: novel, isLoading } = GetNovelData(novelId);
 
+  if (removed)
+    return (
+      <RemovedNovelRow novel={novel} index={index} removeRow={removeRow} />
+    );
   if (isLoading || !novel) return <NovelRowSkeletonLoader index={index} />;
 
   return (
@@ -37,7 +45,7 @@ const NovelRow = ({ novelId, index }) => {
       <div className="flex items-center justify-between overflow-clip">
         <Link
           href={"/novel/" + novel.id}
-          className="flex items-center space-x-2 group rounded-md p-2 hover:bg-neutral-700 fade"
+          className="flex items-center space-x-2 group rounded p-2 hover:bg-neutral-700 fade grow"
         >
           <span>{index + 1}</span>
           <div className="w-[64px] h-[80px]">
@@ -59,39 +67,25 @@ const NovelRow = ({ novelId, index }) => {
             maxProgress={novel.chapters.length}
           />
           <Button className="bg-sky-600">Read</Button>
-          <Menu position="left">
+          <Menu>
             <Menu.Target>
-              <BiDotsVerticalRounded
-                className="rounded-md hover:bg-neutral-700 fade"
-                size={18}
-              />
+              <div className="rounded hover:bg-neutral-700 fade p-2">
+                <BiDotsVerticalRounded size={18} />
+              </div>
             </Menu.Target>
             <Menu.Dropdown>
               <Menu.Item
                 color="red"
                 icon={<BiTrashAlt size={18} />}
                 onClick={() => {
-                  let lib =
-                    JSON.parse(localStorage.getItem("novelLibrary")) || [];
-                  lib.pop(lib.indexOf(novelId));
-                  localStorage.setItem("novelLibrary", JSON.stringify(lib));
+                  removeFromLibrary(novelId);
+                  removeRow(true);
                 }}
               >
                 Remove from Library
               </Menu.Item>
             </Menu.Dropdown>
           </Menu>
-          {/* TODO: Removes incorrect novel */}
-          {/* <BiTrashAlt
-          className="hover:text-red-500 fade"
-          title="Remove from Library"
-          size={18}
-          onClick={() => {
-            let lib = JSON.parse(localStorage.getItem("novelLibrary")) || [];
-            lib.pop(lib.indexOf(novelId));
-            localStorage.setItem("novelLibrary", JSON.stringify(lib));
-          }}
-        /> */}
         </div>
       </div>
     </>
@@ -114,16 +108,89 @@ const NovelRowProgressBar = ({ progress, maxProgress }) => {
   );
 };
 
-// TODO: Finish skeleton loader
-const NovelRowSkeletonLoader = (index) => {
+const RemovedNovelRow = ({ novel, index, removeRow }) => {
   return (
-    <div className="border border-neutral-700 rounded-md p-4 w-full">
-      <div className="animate-pulse flex space-x-4">
-        <div className="rounded-md bg-neutral-700 h-[80px] w-[64px]"></div>
-        <div className="flex-1 space-y-6 py-1">
-          <div className="h-2 bg-neutral-700 rounded my-auto"></div>
+    <>
+      {index != 0 && <Divider size="sm" />}
+      <div className="flex bg-red-500/5 p-2 rounded-md justify-between items-center">
+        <div className="flex space-x-2 text-red-500 items-center">
+          <span>{index + 1}</span>
+          <span className="line-through line-clamp-3">{novel.title}</span>
+        </div>
+        <Button
+          className="border border-red-500 text-red-500 hover:bg-red-500/10 mx-5"
+          onClick={() => {
+            addToLibrary(novel.id, index);
+            removeRow(false);
+          }}
+        >
+          Undo
+        </Button>
+      </div>
+    </>
+  );
+};
+
+const NovelRowSkeletonLoader = ({ index }) => {
+  return (
+    <>
+      {index != 0 && <Divider size="sm" />}
+      <div className="w-full flex justify-between items-center">
+        <div className="flex items-center space-x-2 p-2 grow">
+          <span>{index + 1}</span>
+          <div className="animate-pulse rounded-md bg-neutral-700 h-[80px] w-[64px]" />
+          <div className="animate-pulse flex-1">
+            <div className="space-y-3">
+              <div className="grid grid-cols-3 gap-2">
+                <div className="h-4 bg-neutral-700 rounded col-span-2"></div>
+                <div className="h-4 bg-neutral-700 rounded col-span-1"></div>
+              </div>
+              <div className="h-4 bg-neutral-700 rounded"></div>
+            </div>
+          </div>
+        </div>
+        <div className="animate-pulse flex space-x-2 items-center">
+          <div className="mx-5 hidden md:block relative space-y-1">
+            <div className="w-[250px] h-2 bg-neutral-700 rounded-full" />
+            <div className="w-6 h-3 rounded bg-neutral-700" />
+            <div className="w-10 h-3 rounded bg-neutral-700 right-0 bottom-0 absolute" />
+          </div>
+          <Button className="bg-neutral-700 text-transparent rounded-md hover:bg-neutral-700 pointer-events-none">
+            Read
+          </Button>
+          <div className="w-[34px] h-[34px] bg-neutral-700 rounded-md" />
         </div>
       </div>
-    </div>
+    </>
   );
+};
+
+export const addToLibrary = (novelId: string, index?: number) => {
+  let lib: [string] = getLibrary();
+
+  if (lib.includes(novelId)) return;
+
+  if (!index) lib.push(novelId);
+  else lib.splice(index, 0, novelId);
+
+  localStorage.setItem("novelLibrary", JSON.stringify(lib));
+};
+
+export const removeFromLibrary = (novelId: string) => {
+  let lib: [string] = getLibrary();
+
+  let index = lib.indexOf(novelId);
+  if (index < 0) return;
+
+  lib.splice(index, 1);
+
+  localStorage.setItem("novelLibrary", JSON.stringify(lib));
+};
+
+export const isInLibrary = (novelId) => {
+  return getLibrary().includes(novelId);
+};
+
+const getLibrary = () => {
+  return JSON.parse(localStorage.getItem("novelLibrary")) || [];
 };
