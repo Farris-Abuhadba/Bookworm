bc8ff6434ab9e61337d38776f37d74e23b72d797
Tristan Biesemeier <67628766+trbie@users.noreply.github.com>
2023-10-04 15:17:38 -0700

    navbar with new theme

--- src/components/NovelCard.tsx
+++ src/components/NovelCard.tsx
@@ -11,13 +11,13 @@ const NovelCard = ({ title, image, width, height }: Props) => {
   return (
     <div className="group break-inside-avoid-column mb-5">
       <Image
-        className="rounded-md group-hover:border-neutral-700 border border-transparent fade"
+        className="rounded-md group-hover:border-lavender-600 border border-transparent fade"
         alt={title}
         src={image}
         radius={6}
         withPlaceholder
       />
-      <Text className="mt-1 px-1 rounded-md group-hover:bg-neutral-700 fade">
+      <Text className="mt-1 px-1 rounded-md transparent-button-hover">
         {title}
       </Text>
     </div>
