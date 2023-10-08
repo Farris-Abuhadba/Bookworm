bf2e22f0d33cfee11965627502ed532c8989c20c
trbie <67628766+trbie@users.noreply.github.com>
2023-10-08 14:59:41 -0700

    center loading and error screen

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -1,23 +1,10 @@
 import { Loader } from "@mantine/core";
 import { TiArrowBack } from "react-icons/ti";
 
-interface Props {
-  backUrl?: string;
-}
-
-const LoadingScreen = ({ backUrl }: Props) => {
+const LoadingScreen = () => {
   return (
-    <div className="flex h-screen">
-      {backUrl != null && (
-        <TiArrowBack
-          className="absolute m-2 cursor-pointer hover:text-sky-600"
-          size={32}
-          onClick={() => {
-            location.href = backUrl;
-          }}
-        />
-      )}
-      <Loader className="m-auto" size="xl" />
+    <div className="flex h-screen -mt-24 items-center justify-center">
+      <Loader size="xl" className="stroke-lavender-600" />
     </div>
   );
 };
